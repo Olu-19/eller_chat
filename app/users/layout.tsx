9c63755e4fe64d06a6c9afa96582ff5eef9d8365
Olu-19 <147600581+Olu-19@users.noreply.github.com>
2023-10-11 13:55:36 +0100

    Update layout.tsx

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -2,22 +2,25 @@ import Sidebar from "@/app/components/sidebar/Sidebar";
 import getUsers from "@/app/actions/getUsers";
 import UserList from "@/app/users/components/UserList";
 import { User } from "@prisma/client";
+import getCurrentUser from "../actions/getCurrentUser";
+
+interface UsersLayoutProps {
+  currentUser: User;
+  children: React.ReactNode;
+}
 
 export default async function UsersLayout({
   children,
   currentUser
-}: {
-  children: React.ReactNode;
-  currentUser: User;
-}) {
+}: UsersLayoutProps) {
   const users = await getUsers();
   
   return (
     <Sidebar>
       <div className="h-[100vh]">
         <UserList
-          items={users}
           currentUser={currentUser}
+          items={users}
         />
         {children}
       </div>
